343f55591e2671732f4ba815e748c8cd9e4329fe
Frederico Rhae <fredrhae@gmail.com>
2016-07-04 17:32:53 -0300

    First version of jquery odin project
    First commit of the jquery odin project Etch-a-Sketch.

---
+++ scripts/application.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	var $newparagraph = $('<p> Testando jquery</p>');
+	$('.container').append($newparagraph);
+	
+	var $newparagraph2 = $('<p> Vamo testar mais mermão!!</p>');
+	$('.container').append($newparagraph2);
+});
