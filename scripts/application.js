d6e02ab0ed71e71d0e958910454eaf9cbbd21e28
Frederico Rhae <fredrhae@gmail.com>
2016-07-05 19:24:13 -0300

    Creating grid dinamically
    It was made the code in Javascript using jQuery to create a grid
    dinamically based in the available space of div container in the page.
    The number of squares in grid is set hard coded, but it will change in
    the future version.

--- scripts/application.js
+++ scripts/application.js
@@ -1,7 +1,67 @@
+var containerName = ".container";
+var squareClassName = 'square';
+var numberSquaresInGrid = 16;
+
+// Do the calculations related with div creations
 $(document).ready(function(){
-	var $newparagraph = $('<p> Testando jquery</p>');
-	$('.container').append($newparagraph);
+		
+	var sizeSquare = calculateSizeSquares();
 	
-	var $newparagraph2 = $('<p> Vamo testar mais mermão!!</p>');
-	$('.container').append($newparagraph2);
+	createGrid(sizeSquare);
+
 });
+
+var Size = function(width, height) {
+	this.width = width;
+	this.height = height;
+}
+
+var createGrid = function(size) {
+	
+	for(var i = 1; i <= numberSquaresInGrid; i ++)
+	{
+		var currentRowName = 'row' + i;
+		var $currentRow = $("<div id=" + currentRowName + " style='clear:both'></div>");
+		
+		for(var j = 0; j < numberSquaresInGrid; j ++)
+		{
+			$currentRow.append(createSquare(size));	
+		}
+		
+		$(containerName).append($currentRow);
+	}
+}
+
+var createSquare = function (size) {	
+	var $newDiv = $('<div></div>');
+	$newDiv.addClass(squareClassName);
+	$newDiv.width(size.width).height(size.height);
+	
+	console.log("Created a square of Width = " + size.width + "Height = " + size.height);
+	
+	return $newDiv;
+};
+
+var calculateSizeSquares = function() {
+	var containerHeight = $(containerName).height();
+	var containerWidth = $(containerName).width();
+	
+	// var safetyMargin = (containerWidth * 10) / 100;
+	
+	// var widthSquare = (containerWidth - safetyMargin) / numberSquaresInGrid;
+	
+	var widthBorders = numberSquaresInGrid * 2;
+	
+	var widthMargins = numberSquaresInGrid * 2;
+	
+	console.log("container = " + containerWidth + "| Borders = " + widthBorders + "| Margins = " + widthBorders);
+	
+	var widthSquare = (containerWidth - widthBorders - widthMargins) / numberSquaresInGrid;
+	
+	var sizeSquare = new Size(widthSquare, widthSquare);
+	
+	console.log("Calculated size from square: ");
+	console.log("Height = " + sizeSquare.height + "| Width = " + sizeSquare.width);
+	
+	return sizeSquare;
+}
